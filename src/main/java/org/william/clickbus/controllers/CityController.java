c320b11c97853e72c41f10406611984e926defef
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-10-05 10:15:04 -0300

    Add controller for get city by name

--- src/main/java/org/william/clickbus/controllers/CityController.java
+++ src/main/java/org/william/clickbus/controllers/CityController.java
@@ -1,6 +1,10 @@
 package org.william.clickbus.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.william.clickbus.entites.City;
 import org.william.clickbus.repositories.CityRepository;
@@ -17,17 +21,23 @@ public class CityController {
 
     @GetMapping(value = "/cityall")
     public List<City> getAll() {
-        return repository.findAll();
+        return repository.findAllCities();
     }
 
     @GetMapping(value = "/cityid/{id}")
-    public City findById(@PathVariable Long id) {
-        return repository.findById(id).get();
+    public ResponseEntity<City> findById(@PathVariable Long id) {
+        Optional<City> result = Optional.ofNullable(repository.findByCityId(id));
+
+        if (result.isPresent()) {
+            return ResponseEntity.ok(result.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 
-//    @GetMapping(value = "/cityname/{name}")
-//    public City findByName(@PathVariable String name) {
-//    }
+    @GetMapping(value = "/cityname/{name}")
+    public City findByName(@PathVariable String name) {
+        return repository.findCityByName(name);
+    }
 
     @PostMapping(value = "/citycreate")
     public City createCity(@RequestBody City city) {
--- src/main/java/org/william/clickbus/repositories/CityRepository.java
+++ src/main/java/org/william/clickbus/repositories/CityRepository.java
@@ -1,7 +1,25 @@
 package org.william.clickbus.repositories;
 
+import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.william.clickbus.entites.City;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CityRepository extends JpaRepository<City, Long> {
+
+    // Get all cities
+    @Query(value = "SELECT * FROM tb_city", nativeQuery = true)
+    List<City> findAllCities();
+
+    // Get city by id
+    @Query(value = "SELECT * FROM tb_city WHERE id = :id", nativeQuery = true)
+    City findByCityId(@Param("id") Long id);
+
+    // Get city by name
+    @Query(value = "SELECT * FROM tb_city WHERE name = :name", nativeQuery = true)
+    City findCityByName(@Param("name") String name);
 }
